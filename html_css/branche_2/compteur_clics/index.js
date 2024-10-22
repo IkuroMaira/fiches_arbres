7e6b8ae1e327a405c1fd168f9ac97281abc84a8d
IkuroMaira <gwen.bussac@gmail.com>
2024-10-22 16:35:58 +0200

    update exos branche 2

--- html_css/branche_2/compteur_clics/index.js
+++ html_css/branche_2/compteur_clics/index.js
@@ -1,5 +1,18 @@
+// La constante qui récupère le bouton dans le HTML
 const getCounter = document.getElementById("counter");
 
-let counter = 0;
+// Le variable de départ pour compter les cliques
+let count = 0;
 
 // Ici on ajoute un évènement sur le bouton
+getCounter.addEventListener('click', () => {
+    // à chaque clique on incrément
+    for (let i = 0; i < 12 ; i++) {
+        get
+    }
+    // on modifie le texte à l'intérieur du bouton avec le nouvelle valeur de cont
+})
+
+// TEST_______________________________________
+let getBtn = document.getElementById("test");
+getBtn.addEventListener("click", displayDate);
--- html_css/branche_2/horoscope_devs/index.js
+++ html_css/branche_2/horoscope_devs/index.js
@@ -27,31 +27,102 @@ for (const item of horoscope) {
     //   <p>Description</p>
     // </article>
 
-    console.log(item.sign, item.description);
+    console.log("console pas de moi: ",item.sign, item.description);
 
     // J'ai récupéré la div avec l'id "horoscope"
-    const getDiv = document.getElementById("horoscope");
+    // const getDiv = document.getElementById("horoscope");
     // Cela permet de sélectionner l'élément HTML où tu vas ajouter dynamiquement les articles contenant les signes et descriptions.
 
     // On boucle sur chaque item
     // createElement est une méthode de JavaScript utilisée pour créer un nouvel élément HTML dynamiquement.
     // Cet élément n'est pas encore dans le DOM, mais tu peux le manipuler (par exemple, lui ajouter du texte, des attributs) avant de l'insérer dans la page.
-    const article = document.createElement('article'); // Ici on créé l'élément article. Contiendra le signe et la description.
-    const h2 = document.createElement('h2'); // Ici ça créé l'élément h2 pour le signe
-    const p = document.createElement('p'); // Ici ça créé l'élément p pour la description
+    // const article = document.createElement('article'); // Ici on créé l'élément article. Contiendra le signe et la description.
+    // const h2 = document.createElement('h2'); // Ici ça créé l'élément h2 pour le signe
+    // const p = document.createElement('p'); // Ici ça créé l'élément p pour la description
 
     // Ici on ajouter le texte au h2 et p
-    h2.innerText = item.sign; // Mettre le signe astrologique comme texte du h2
-    p.innerText = item.description;
+    // h2.innerText = item.sign; // Mettre le signe astrologique comme texte du h2
+    // p.innerText = item.description;
 
     // On ajoute le h2 et le p à l'élément article
     // appendChild est une méthode qui permet d'ajouter un élément enfant à un élément parent dans le DOM.
     // En d'autres termes, cela te permet d'insérer dynamiquement un élément (précédemment créé avec createElement, par exemple) dans le DOM.
-    article.appendChild(h2);
-    article.appendChild(p);
+    // article.appendChild(h2);
+    // article.appendChild(p);
 
     // On ajoute l'article à la div horoscope
-    getDiv.appendChild(article);
+    // getDiv.appendChild(article);
 
     // Les éléments h2 et p sont d'abord ajoutés à l'élément article, puis l'article entier est ajouté à la div contenant l'horoscope.
+
+
+    // TEST 1
+    // const getDiv2 = document.getElementById("horoscope");
+    // const h2 = document.createElement('h2')
+    // h2.innerText = item;
+    // getDiv2.appendChild(h2);
+    // Nous affiche juste des objets. Mais bonne nouvelle je vois une liste
+
+    // TEST 2
+    // const getDiv3 = document.getElementById("horoscope");
+    // const article = document.createElement('article');
+    // const p = document.createElement('p')
+    // On créé un paragraphe
+    // article.innerText = item;
+    // item = le tableau d'objets
+    // p.innerText = item.sign;
+    // on n'arrive pas à récupérer les signes astros
+    // p.innerText = item.description
+    // on arrive à récupérer les descriptions des objets
+    // peut-être je créer un conflit avec la ligne précédente
+    // console.log(getDiv3.appendChild(article))
+    // S'affiche dans le console.log avec les mot "object"
+    // console.log(getDiv3.appendChild(p));
+    // S'affiche dans le console.log
+    // On affiche toujours des objets et toujours en liste avec jusste item pour les 'innerText'
+
+    // TEST 3
+    // const getDiv4 = document.getElementById("horoscope");
+    // Je récupère la div
+    // const article = document.createElement('article');
+    // const h2 = document.createElement('h2');
+    // const p = document.createElement('p');
+    // Je créé un élement article, titre h2 et paragraphe
+
+    // article.innerText = item;
+    // h2.innerText = item.sign;
+    // Là j'arrive à récupérer les signes astros
+    // p.innerText = item.description;
+
+    // Les enfants !!!!!!!
+    // console.log(getDiv4.appendChild(article));
+    // console.log(getDiv4.appendChild(h2));
+    // console.log(getDiv4.appendChild(p));
+    // Je mets article, h2 et p comme enfant de la div "horoscope"
+
+    // console.log(getDiv4)
+    // S'affiche les objets avec toutes les infos
+
+    // Dans cette partie j'arrive à afficher le h2 et la description mais je n'ai toujours pas la notion d'article qui s'affiche (h2 et description dans article)
+    // const article = document.createElement('article');
+
+    // getDiv4.appendChild(article).appendChild(p);
+    // getDiv4.appendChild(article);
+    // article.appendChild(h2);
+    // article.appendChild(p);
+
+    // PROPRE & RÉPONSE FINALE
+    const getDiv4 = document.getElementById("horoscope");
+    // Ici
+
+    const article = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+
+    h2.innerText = item.sign;
+    p.innerText = item.description;
+
+    article.appendChild(h2);
+    article.appendChild(p);
+    getDiv4.appendChild(article);
 }
